ebdaca9b6f9f9afce3b6fc3a86ebdff06ee3ead3
Tolushawlar <tolushaw.it@gmail.com>
2023-11-20 12:47:42 +0100

    jwt-implemented

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .tokens import create_jwt_pair_for_user
 
-# Create your views here.
+# display success login info with the created token data
+# tokens = create_jwt_pair_for_user(user)
+# response = {"message": "Login Successful", "tokens": tokens}
--- riderexpert/settings.py
+++ riderexpert/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from datetime import timedelta
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "django_extensions",
+    "rest_framework_simplejwt",
     "accounts"
 ]
 
@@ -124,6 +127,14 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         # Other authentication classes as needed
     ),
+}
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    'SIGNING_KEY': SECRET_KEY,
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
 # Internationalization
