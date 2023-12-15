2707573faae59d91bba66e1e3585abc0400e259b
Rider Account <tech.account@riderexpert.com.ng>
2023-12-15 07:30:04 +0000

    remove feature added

--- orders/urls.py
+++
@@ -1,7 +0,0 @@
-from . import assign_order_to_rider
-from django.urls import path
-
-
-urlpatterns = [
-    path('/api/orders/<int:order_id>/rider/<rider_id>/', assign_order_to_rider, name='assign-order-to-rider'),
-]
