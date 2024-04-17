166ada5231e1e67d9de8da2b83adc79baae26bc7
Pandas <temdire.adams>
2024-04-17 16:24:41 +0100

    changes

--- riderexpert/settings.py
+++ riderexpert/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_extensions",
     "rest_framework_simplejwt",
+    "corsheaders",
     "accounts",
     "orders",
     "adrf",
@@ -61,6 +62,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -69,6 +72,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = ["https://*.ngrok.io", "http://localhost:3000"]
 
 ROOT_URLCONF = "riderexpert.urls"
 
